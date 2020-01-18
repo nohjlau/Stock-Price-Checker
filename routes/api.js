4953071becb9809f9fc775f8daf393a2abf8191c
John Lau <nohjlau@gmail.com>
2020-01-17 17:07:49 -0800

    Implemented single stock data fetch

--- routes/api.js
+++ routes/api.js
@@ -10,6 +10,7 @@
 
 var expect = require('chai').expect;
 var mongoose = require('mongoose');
+var fetch = require('node-fetch');
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, function (err) {
   if (err) throw err;
@@ -28,48 +29,71 @@ module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      // TODO: Implement Price lookup
-
       let ip = req.ip || req.connection.remoteAddress;
-      let query = req.query.stock.toUpperCase();
+      let query;
+      if(typeof(req.query.stock) === 'string') {
+        query = req.query.stock.toUpperCase();
+      } else {
+        query = [];
+        req.query.stock.forEach(d => {
+          query.push(d);
+        });
+      }
       let likes = req.query.like === 'true'  ? 1 : 0;
 
-      Ticker.findOne({symbol: query}, (err, res) => {
-        if (err) throw err;
-        if(res !== null) {
-          console.log('Ticker already exists!');
-          if(likes) {
-            let ipExists = false;
-            res.ips.forEach((d) => {
-              ipExists = true;
-              console.log("IP has already liked!");
-            });
-            if(!ipExists){
-              console.log("IP doesn't exist. Increment likes");
-              res.ips.push(ip);
-              res.save(function(err, data) {
-                if(err) throw err;
-              })
+      if(typeof(query) === 'string') { // ONE TICKER
+        fetch('https://repeated-alpaca.glitch.me/v1/stock/' + query + '/quote')
+          .then(function(res) {
+            return res.json();
+          })
+          .then(function(json) {
+            if(json.latestPrice == null) {
+              res.json({stockdata: { error: "external source error"}});
+              console.log('Invalid stock ticker!');
+            } else {
+              res.json({stockdata: {stock: query, price: json.latestPrice, likes: likes}});
+              Ticker.findOne({symbol: query}, (err, res) => {
+                if (err) throw err;
+                if(res !== null) {
+                  console.log('Ticker already exists!');
+                  if(likes) {
+                    let ipExists = false;
+                    res.ips.forEach((d) => {
+                      ipExists = true;
+                      console.log("IP has already liked!");
+                    });
+                    if(!ipExists){
+                      console.log("IP doesn't exist. Increment likes");
+                      res.ips.push(ip);
+                      res.save(function(err, data) {
+                        if(err) throw err;
+                      })
+                    }
+                  }
+                } else {
+                  var newTicker = new Ticker({
+                    symbol: query,
+                    likes: likes,
+                  })
+        
+                  if(likes) {
+                    newTicker.ips = ip;
+                  }
+        
+                  newTicker.save(function(err) {
+                    if (err) throw err;
+                    console.log('Ticker successfully saved.');
+                  })
+                }
+              });
             }
-          }
-        } else {
-          var newTicker = new Ticker({
-            symbol: query,
-            likes: likes,
-          })
+          });
+      } else { // MULTIPLE TICKERS
+      
+      
+      
+      }
 
-          if(likes) {
-            newTicker.ips = ip;
-          }
-
-          newTicker.save(function(err) {
-            if (err) throw err;
-            console.log('Ticker successfully saved.');
-          })
-        }
-      });
-
-      res.json({stockdata: {stock: query, price: 0, likes: likes}});
-
-    }); 
+    });
+    
 };
